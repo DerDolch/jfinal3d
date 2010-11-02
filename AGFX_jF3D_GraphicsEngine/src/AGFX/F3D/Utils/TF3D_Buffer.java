8065c2995ab8f671515b119d77e47fc26a26b030
o.kollar@gmail.com <o.kollar@gmail.com@997aefa7-b983-09b2-ede8-16dce480a187>
2010-11-02 14:12:56 +0000

    Fix: memory leak

--- AGFX_jF3D_GraphicsEngine/src/AGFX/F3D/Utils/TF3D_Buffer.java
+++ AGFX_jF3D_GraphicsEngine/src/AGFX/F3D/Utils/TF3D_Buffer.java
@@ -1,34 +1,83 @@
 package AGFX.F3D.Utils;
 
+import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 
+import javax.vecmath.Vector2f;
 import javax.vecmath.Vector3f;
+import javax.vecmath.Vector4f;
 
 import org.lwjgl.BufferUtils;
 
 public class TF3D_Buffer
 {
-	public static FloatBuffer fB = BufferUtils.createFloatBuffer(4);
-	public static IntBuffer iB = BufferUtils.createIntBuffer(4);
+	public static FloatBuffer fB = BufferUtils.createFloatBuffer(16);
+	public static IntBuffer iB = BufferUtils.createIntBuffer(16);
+	public static ByteBuffer bB = BufferUtils.createByteBuffer(16);
 	
 	
 	public FloatBuffer Float(float[] f)
 	{
-		fB.clear();
-		return (FloatBuffer) fB.put(f).flip();
+		TF3D_Buffer.fB.clear();
+		TF3D_Buffer.fB.put(f).flip();
+		
+		return TF3D_Buffer.fB; 
+		
+	}
+	
+	public FloatBuffer Float(Vector2f v)
+	{
+		TF3D_Buffer.fB.clear();
+		TF3D_Buffer.fB.put(v.x);
+		TF3D_Buffer.fB.put(v.y);
+		TF3D_Buffer.fB.flip();
+		
+		return TF3D_Buffer.fB;
 		
 	}
 	
 	public FloatBuffer Float(Vector3f v)
 	{
-		fB.clear();
-		fB.put(v.x);
-		fB.put(v.y);
-		fB.put(v.z);
-		fB.flip();
+		TF3D_Buffer.fB.clear();
+		TF3D_Buffer.fB.put(v.x);
+		TF3D_Buffer.fB.put(v.y);
+		TF3D_Buffer.fB.put(v.z);
+		TF3D_Buffer.fB.flip();
 		
-		return fB;
+		return TF3D_Buffer.fB;
 		
 	}
+	
+	public FloatBuffer Float(Vector4f v)
+	{
+		TF3D_Buffer.fB.clear();
+		TF3D_Buffer.fB.put(v.x);
+		TF3D_Buffer.fB.put(v.y);
+		TF3D_Buffer.fB.put(v.z);
+		TF3D_Buffer.fB.put(v.w);
+		TF3D_Buffer.fB.flip();
+		
+		return TF3D_Buffer.fB;
+		
+	}
+	
+	public IntBuffer Int(int[] i)
+	{
+		TF3D_Buffer.iB.clear();
+		TF3D_Buffer.iB.put(i).flip();
+		
+		return TF3D_Buffer.iB ;
+	}
+	
+	public ByteBuffer Byte(byte[] b)
+	{
+		TF3D_Buffer.bB = BufferUtils.createByteBuffer(b.length);
+		TF3D_Buffer.bB.clear();
+		TF3D_Buffer.bB.put(b);
+		TF3D_Buffer.bB.flip();
+		
+		return TF3D_Buffer.bB;
+	}
+	
 }
