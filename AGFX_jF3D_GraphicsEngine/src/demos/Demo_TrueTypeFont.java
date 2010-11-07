c0ff3f828a21ba24ac302d5de0b2128ee2a2b088
o.kollar@gmail.com <o.kollar@gmail.com@997aefa7-b983-09b2-ede8-16dce480a187>
2010-11-07 19:53:54 +0000

    + Fix TTF font rendering + add to Demo_TrueTypeFont - loading user defined font

--- AGFX_jF3D_GraphicsEngine/src/demos/Demo_TrueTypeFont.java
+++ AGFX_jF3D_GraphicsEngine/src/demos/Demo_TrueTypeFont.java
@@ -3,7 +3,23 @@
  */
 package demos;
 
+import static org.lwjgl.opengl.GL11.GL_ALPHA_TEST;
+import static org.lwjgl.opengl.GL11.GL_BLEND;
+import static org.lwjgl.opengl.GL11.GL_CULL_FACE;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_TEST;
+import static org.lwjgl.opengl.GL11.GL_GREATER;
+import static org.lwjgl.opengl.GL11.glAlphaFunc;
+import static org.lwjgl.opengl.GL11.glDepthMask;
+import static org.lwjgl.opengl.GL11.glDisable;
+
 import java.awt.Font;
+import java.awt.FontFormatException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.lwjgl.opengl.GL11;
 
 import AGFX.F3D.F3D;
 import AGFX.F3D.AppWrapper.TF3D_AppWrapper;
@@ -17,32 +33,31 @@ import AGFX.F3D.Font.TF3D_Font_TTF;
 public class Demo_TrueTypeFont extends TF3D_AppWrapper
 {
 
-	TF3D_Font_TTF  ttf;
-	Font 			font;
-	
+	TF3D_Font_TTF ttf;
+	Font          font;
+
+	TF3D_Font_TTF ttf_bumbazoo;
+	Font          font_bumbazoo;
+
 	public Demo_TrueTypeFont()
-	{		
+	{
 	}
-	
-	
+
 	@Override
 	public void onConfigure()
 	{
 		try
 		{
-			
+
 			F3D.Config = new TF3D_Config();
-			
+
 			F3D.Config.r_display_width = 800;
 			F3D.Config.r_display_height = 600;
 			F3D.Config.r_fullscreen = false;
 			F3D.Config.r_display_vsync = true;
-			F3D.Config.r_display_title = "jFinal3D Graphics Engine 2010 - "+ this.getClass().getName();
+			F3D.Config.r_display_title = "jFinal3D Graphics Engine 2010 - " + this.getClass().getName();
 
 			super.onConfigure();
-
-			
-			
 
 		} catch (Exception e)
 		{
@@ -53,37 +68,69 @@ public class Demo_TrueTypeFont extends TF3D_AppWrapper
 	@Override
 	public void onInitialize()
 	{
+		// load system TTF font
 		Font font = new Font("Verdana", Font.BOLD, 32);
 		this.ttf = new TF3D_Font_TTF(font, true);
+
+		// load user defined font
+
+		
+
+		try
+		{
+			File f = new File("media/ttf/BUMBAZO.TTF");
+			FileInputStream in = new FileInputStream(f);
+			font_bumbazoo = Font.createFont(Font.TRUETYPE_FONT, in);
+			font_bumbazoo = font_bumbazoo.deriveFont(24f);
+			
+		} catch (FontFormatException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		this.ttf_bumbazoo = new TF3D_Font_TTF(font_bumbazoo, true);
+		
 	}
-	
+
 	@Override
 	public void onUpdate3D()
 	{
 	}
-	
-	
+
 	@Override
 	public void onUpdate2D()
 	{
-		
-		
-		this.ttf.drawString(50,50, "Demo of Tryetype font", 1f, 1f);
-		
-	}
 	
+		
+		F3D.Textures.DeactivateLayers();
+		F3D.Textures.ActivateLayer(0);
+		GL11.glColor4f(1,1,1,1);		
+		this.ttf.drawString(50, 50, "Demo of Tryetype font", 1f, 1f);
+		
+		// create shadow
+		GL11.glColor4f(0,0,0,0.5f);
+		this.ttf_bumbazoo.drawString(52, 152, "Demo of User Tryetype font", 1f, 1f);
+		// draw font
+		GL11.glColor4f(1,1,0,1);
+		this.ttf_bumbazoo.drawString(50, 150, "Demo of User Tryetype font", 1f, 1f);
+	}
+
 	@Override
 	public void OnDestroy()
 	{
-		
+
 	}
-	
-	
+
 	public static void main(String[] args)
 	{
-		
-		new Demo_TrueTypeFont().Execute();		
-		System.exit(0); 
+
+		new Demo_TrueTypeFont().Execute();
+		System.exit(0);
 
 	}
 
