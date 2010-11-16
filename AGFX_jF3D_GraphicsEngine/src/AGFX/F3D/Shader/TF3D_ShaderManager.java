32a828ea5ebc7b21fa79df5279bda7d5155e5687
o.kollar@gmail.com <o.kollar@gmail.com@997aefa7-b983-09b2-ede8-16dce480a187>
2010-11-16 11:37:07 +0000

    + add control ON/OFF control for shaders presets. Now is possible enable/disable all shaders separately

--- AGFX_jF3D_GraphicsEngine/src/AGFX/F3D/Shader/TF3D_ShaderManager.java
+++ AGFX_jF3D_GraphicsEngine/src/AGFX/F3D/Shader/TF3D_ShaderManager.java
@@ -16,18 +16,18 @@ import AGFX.F3D.Callback.TF3D_ShaderCallback;
  */
 public class TF3D_ShaderManager
 {
-	public ArrayList<TF3D_Shader> items;
-	public TF3D_Shader            shader_diffuse;
-	public TF3D_Shader            shader_phong;
-	public TF3D_Shader            shader_envmap;
-	public TF3D_Shader            shader_glow;
-	public TF3D_Shader            shader_posterize;
-	public TF3D_Shader            shader_dream;
-	public TF3D_Shader            shader_warp;
-	public TF3D_Shader            shader_blur_h;
-	public TF3D_Shader            shader_blur_v;
-	public TF3D_Shader            shader_gaussian_v;
-	public TF3D_Shader            shader_gaussian_h;
+	public ArrayList<TF3D_Shader>	items;
+	public TF3D_Shader				shader_diffuse;
+	public TF3D_Shader				shader_phong;
+	public TF3D_Shader				shader_envmap;
+	public TF3D_Shader				shader_glow;
+	public TF3D_Shader				shader_posterize;
+	public TF3D_Shader				shader_dream;
+	public TF3D_Shader				shader_warp;
+	public TF3D_Shader				shader_blur_h;
+	public TF3D_Shader				shader_blur_v;
+	public TF3D_Shader				shader_gaussian_v;
+	public TF3D_Shader				shader_gaussian_h;
 
 	public TF3D_ShaderManager()
 	{
@@ -271,121 +271,140 @@ public class TF3D_ShaderManager
 
 	public void InitPresets()
 	{
-		
+
 		if ((F3D.Config.use_shaders) & (F3D.Extensions.GLSL_VertexShader) & (F3D.Extensions.GLSL_FragmenShader))
 		{
 			// Shader: DIFFUSE
 
-			shader_diffuse = new TF3D_Shader("DIFFUSE");
-			shader_diffuse.Load("abstract::f3d_diffuse.vert", "abstract::f3d_diffuse.frag");
-			shader_diffuse.AddUniform1i("BaseMap", 0);
-			this.Add(shader_diffuse);
+			if (F3D.Config.shd_load_diffuse)
+			{
+				shader_diffuse = new TF3D_Shader("DIFFUSE");
+				shader_diffuse.Load("abstract::f3d_diffuse.vert", "abstract::f3d_diffuse.frag");
+				shader_diffuse.AddUniform1i("BaseMap", 0);
+
+				this.Add(shader_diffuse);
+			}
 
 			// Shader: PHONG
 
-			shader_phong = new TF3D_Shader("PHONG");
-			shader_phong.Load("abstract::f3d_phong.vert", "abstract::f3d_phong.frag");
-			shader_phong.AddUniform1i("BaseMap", 0);
-			shader_phong.AddUniform4f("fvSpecular", 0.7f, 0.7f, 0.7f, 1f);
-			shader_phong.AddUniform4f("fvDiffuse", 0.7f, 0.7f, 0.7f, 1f);
-			shader_phong.AddUniform4f("fvAmbient", 0.1f, 0.1f, 0.1f, 1f);
-			shader_phong.AddUniform1f("fSpecularPower", 100f);
-			shader_phong.AddUniform3f("fvLightPosition", -3f, 3f, 3f);
-			shader_phong.AddUniform3f("fvEyePosition", 2f, 2f, 2f);
+			if (F3D.Config.shd_load_phong)
+			{
+				shader_phong = new TF3D_Shader("PHONG");
+				shader_phong.Load("abstract::f3d_phong.vert", "abstract::f3d_phong.frag");
+				shader_phong.AddUniform1i("BaseMap", 0);
+				shader_phong.AddUniform4f("fvSpecular", 0.7f, 0.7f, 0.7f, 1f);
+				shader_phong.AddUniform4f("fvDiffuse", 0.7f, 0.7f, 0.7f, 1f);
+				shader_phong.AddUniform4f("fvAmbient", 0.1f, 0.1f, 0.1f, 1f);
+				shader_phong.AddUniform1f("fSpecularPower", 100f);
+				shader_phong.AddUniform3f("fvLightPosition", -3f, 3f, 3f);
+				shader_phong.AddUniform3f("fvEyePosition", 2f, 2f, 2f);
 
-			this.Add(shader_phong);
+				this.Add(shader_phong);
+			}
 
 			// Shader: ENVMAP
+			if (F3D.Config.shd_load_envmap)
+			{
+				shader_envmap = new TF3D_Shader("ENVMAP");
+				shader_envmap.Load("abstract::f3d_envmap.vert", "abstract::f3d_envmap.frag");
+				shader_envmap.AddUniform1i("BaseMap", 0);
+				shader_envmap.AddUniform1i("EnvMap", 1);
+				shader_envmap.AddUniform3f("BaseColor", 0.7f, 0.7f, 0.7f);
+				shader_envmap.AddUniform1f("MixRatio", 0.8f);
+				shader_envmap.AddUniform3f("LightPos", 13f, 13f, 13f);
 
-			shader_envmap = new TF3D_Shader("ENVMAP");
-			shader_envmap.Load("abstract::f3d_envmap.vert", "abstract::f3d_envmap.frag");
-			shader_envmap.AddUniform1i("BaseMap", 0);
-			shader_envmap.AddUniform1i("EnvMap", 1);
-			shader_envmap.AddUniform3f("BaseColor", 0.7f, 0.7f, 0.7f);
-			shader_envmap.AddUniform1f("MixRatio", 0.8f);
-			shader_envmap.AddUniform3f("LightPos", 13f, 13f, 13f);
-
-			this.Add(shader_envmap);
+				this.Add(shader_envmap);
+			}
 
 			// Shader: GLOW
+			if (F3D.Config.shd_load_glow)
+			{
+				shader_glow = new TF3D_Shader("GLOW");
+				shader_glow.Load("abstract::f3d_glow.vert", "abstract::f3d_glow.frag");
+				shader_glow.AddUniform1i("BaseMap", 0);
+				shader_glow.AddUniform1i("GlowMap", 1);
 
-			shader_glow = new TF3D_Shader("GLOW");
-			shader_glow.Load("abstract::f3d_glow.vert", "abstract::f3d_glow.frag");
-			shader_glow.AddUniform1i("BaseMap", 0);
-			shader_glow.AddUniform1i("GlowMap", 1);
+				this.Add(shader_glow);
+			}
 
-			this.Add(shader_glow);
-			
-			
 			// Shader: POSTERIZE
-			
-			shader_posterize = new TF3D_Shader("POSTERIZE");
-			shader_posterize.Load("abstract::f3d_posterize.vert", "abstract::f3d_posterize.frag");
-			shader_posterize.AddUniform1i("sceneTex", 0);
-			shader_posterize.AddUniform1f("gamma", 0.6f);
-			shader_posterize.AddUniform1f("numColors", 8.0f);
-			this.Add(shader_posterize);
-			
+
+			if (F3D.Config.shd_load_posterize)
+			{
+				shader_posterize = new TF3D_Shader("POSTERIZE");
+				shader_posterize.Load("abstract::f3d_posterize.vert", "abstract::f3d_posterize.frag");
+				shader_posterize.AddUniform1i("sceneTex", 0);
+				shader_posterize.AddUniform1f("gamma", 0.6f);
+				shader_posterize.AddUniform1f("numColors", 8.0f);
+				this.Add(shader_posterize);
+			}
+
 			// Shader: DREAM
-			
-			shader_dream = new TF3D_Shader("DREAM");
-			shader_dream.Load("abstract::f3d_dream.vert", "abstract::f3d_dream.frag");
-			shader_dream.AddUniform1i("sceneTex", 0);
-			this.Add(shader_dream);
-			
-			
+			if (F3D.Config.shd_load_dream)
+			{
+				shader_dream = new TF3D_Shader("DREAM");
+				shader_dream.Load("abstract::f3d_dream.vert", "abstract::f3d_dream.frag");
+				shader_dream.AddUniform1i("sceneTex", 0);
+				this.Add(shader_dream);
+			}
+
 			// Shader: DREAM
-			
-			shader_warp = new TF3D_Shader("WARP");
-			shader_warp.Load("abstract::f3d_warp.vert", "abstract::f3d_warp.frag");
-			shader_warp.AddUniform1i("SceneMap", 0);
-			shader_warp.AddUniform1i("NoiseMap", 1);
-			shader_warp.AddUniform1f("Speed", 0.05f);
-			shader_warp.AddUniformEvent("TIMER", F3D.SHADER_EVENT_TIMER);
-			this.Add(shader_warp);
-			
-			
+			if (F3D.Config.shd_load_warp)
+			{
+				shader_warp = new TF3D_Shader("WARP");
+				shader_warp.Load("abstract::f3d_warp.vert", "abstract::f3d_warp.frag");
+				shader_warp.AddUniform1i("SceneMap", 0);
+				shader_warp.AddUniform1i("NoiseMap", 1);
+				shader_warp.AddUniform1f("Speed", 0.05f);
+				shader_warp.AddUniformEvent("TIMER", F3D.SHADER_EVENT_TIMER);
+				this.Add(shader_warp);
+			}
+
 			// Shader: BLUR_H
-			
-			shader_blur_h = new TF3D_Shader("BLUR_H");
-			shader_blur_h.Load("abstract::f3d_blur_h.vert", "abstract::f3d_blur_h.frag");
-			shader_blur_h.AddUniform1i("sceneTex", 0);
-			shader_blur_h.AddUniform1f("rt_w", 256);
+			if (F3D.Config.shd_load_blur_h)
+			{
+				shader_blur_h = new TF3D_Shader("BLUR_H");
+				shader_blur_h.Load("abstract::f3d_blur_h.vert", "abstract::f3d_blur_h.frag");
+				shader_blur_h.AddUniform1i("sceneTex", 0);
+				shader_blur_h.AddUniform1f("rt_w", 256);
 
-
-			
-			this.Add(shader_blur_h);
+				this.Add(shader_blur_h);
+			}
 
 			// Shader: BLUR_V
-			
-			shader_blur_v = new TF3D_Shader("BLUR_V");
-			shader_blur_v.Load("abstract::f3d_blur_v.vert", "abstract::f3d_blur_v.frag");
-			shader_blur_v.AddUniform1i("sceneTex", 0);
-			shader_blur_v.AddUniform1f("rt_h", 256);
 
-			
-			this.Add(shader_blur_v);
-			
-			
+			if (F3D.Config.shd_load_blur_v)
+			{
+				shader_blur_v = new TF3D_Shader("BLUR_V");
+				shader_blur_v.Load("abstract::f3d_blur_v.vert", "abstract::f3d_blur_v.frag");
+				shader_blur_v.AddUniform1i("sceneTex", 0);
+				shader_blur_v.AddUniform1f("rt_h", 256);
+
+				this.Add(shader_blur_v);
+			}
+
 			// Shader: GAUSSIAN_V
-			
-			shader_gaussian_v = new TF3D_Shader("GAUSSIAN_V");
-			shader_gaussian_v.Load("abstract::f3d_gaussian.vert", "abstract::f3d_gaussian_v.frag");
-			shader_gaussian_v.AddUniform1i("sceneTex", 0);
-			shader_gaussian_v.AddUniform1f("offset", 0.0075f);
-			
-			
-			this.Add(shader_gaussian_v);
-			
+			if (F3D.Config.shd_load_gaussian_v)
+			{
+				shader_gaussian_v = new TF3D_Shader("GAUSSIAN_V");
+				shader_gaussian_v.Load("abstract::f3d_gaussian.vert", "abstract::f3d_gaussian_v.frag");
+				shader_gaussian_v.AddUniform1i("sceneTex", 0);
+				shader_gaussian_v.AddUniform1f("offset", 0.0075f);
+
+				this.Add(shader_gaussian_v);
+			}
+
 			// Shader: GAUSSIAN_H
-			
-			shader_gaussian_h = new TF3D_Shader("GAUSSIAN_H");
-			shader_gaussian_h.Load("abstract::f3d_gaussian.vert", "abstract::f3d_gaussian_h.frag");
-			shader_gaussian_h.AddUniform1i("sceneTex", 0);
-			shader_gaussian_h.AddUniform1f("offset", 0.0075f);
-			
-			this.Add(shader_gaussian_h);
-			
+			if (F3D.Config.shd_load_gaussian_h)
+			{
+				shader_gaussian_h = new TF3D_Shader("GAUSSIAN_H");
+				shader_gaussian_h.Load("abstract::f3d_gaussian.vert", "abstract::f3d_gaussian_h.frag");
+				shader_gaussian_h.AddUniform1i("sceneTex", 0);
+				shader_gaussian_h.AddUniform1f("offset", 0.0075f);
+
+				this.Add(shader_gaussian_h);
+			}
+
 		}
 	}
 }
