58a81a902f4c6eeb847fe5c95f9da14252e2453a
o.kollar@gmail.com <o.kollar@gmail.com@997aefa7-b983-09b2-ede8-16dce480a187>
2010-11-21 21:37:32 +0000

    + prepare class F3D for Slick2D game development + added SlickWrapper + added Particle Editor to tools for Slick

---
+++ AGFX_jF3D_GraphicsEngine/src/AGFX/F3D/Slick/TF3D_Slick.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package AGFX.F3D.Slick;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+
+/**
+ * @author AndyGFX
+ *
+ */
+public class TF3D_Slick
+{
+	public GameContainer container;
+	public Graphics graphics;
+	
+	public void TF3D_Slick()
+	{		
+	}
+	
+	public void Initialize()
+	{
+		this.graphics = new Graphics();
+	}
+}
