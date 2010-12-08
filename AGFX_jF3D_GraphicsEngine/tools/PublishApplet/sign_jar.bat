8cd40a2dda9b546265737c93f1d2ab56d821bc5f
o.kollar@gmail.com <o.kollar@gmail.com@997aefa7-b983-09b2-ede8-16dce480a187>
2010-12-08 21:06:23 +0000

    + prepare tools/PublishApplet packages to release applet

--- AGFX_jF3D_GraphicsEngine/tools/PublishApplet/sign_jar.bat
+++ AGFX_jF3D_GraphicsEngine/tools/PublishApplet/sign_jar.bat
@@ -3,3 +3,4 @@ call "c:\Program Files (x86)\Java\jdk1.6.0_21\bin\jarsigner.exe" -keystore andyg
 call "c:\Program Files (x86)\Java\jdk1.6.0_21\bin\jarsigner.exe" -keystore andygfx.store -storepass gfxtool -keypass gfxtool vecmath.jar andygfx
 call "c:\Program Files (x86)\Java\jdk1.6.0_21\bin\jarsigner.exe" -keystore andygfx.store -storepass gfxtool -keypass gfxtool jbullet.jar andygfx
 call "c:\Program Files (x86)\Java\jdk1.6.0_21\bin\jarsigner.exe" -keystore andygfx.store -storepass gfxtool -keypass gfxtool slick-util.jar andygfx
+call "c:\Program Files (x86)\Java\jdk1.6.0_21\bin\jarsigner.exe" -keystore andygfx.store -storepass gfxtool -keypass gfxtool slick.jar andygfx
